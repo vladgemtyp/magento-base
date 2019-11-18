1cb0e4496fbc582e00b11c41e38a8d455ce83c81
Vlad Kozak <vlad.gem.typ@gmail.com>
2019-11-18 11:45:56 +0200

    init

---
+++ app/code/community/Popov/Base/Helper/Url.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Base url helper
+ *
+ * @category Popov
+ * @package Popov_Base
+ * @author Popov Sergiy <popov.serhii@gmail.com>
+ * @datetime: 10.06.14 18:09
+ */
+
+class Popov_Base_Helper_Url extends Mage_Core_Helper_Abstract {
+
+}
